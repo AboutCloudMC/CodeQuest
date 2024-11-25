4b3d552ae1cfb9094486d84cac39bbe2d5462e14
AboutCloudMC <aboutcloudmc@gmail.com>
2024-11-25 12:14:29 +0000

    algo 1

--- Problem68-ColorWheel/app.py
+++ Problem68-ColorWheel/app.py
@@ -1,15 +1,29 @@
 prim = ["yellow", "red", "blue"]
 
 colors = {
+    "red-violet": [prim[1], "violet"],
     "violet": [prim[1], prim[2]],
+    "blue-violet": [prim[2], "violet"],
+    "blue-green": [prim[2], prim[0]],
+    "green": [prim[2], prim[1]],
+    "yellow-green": [prim[0], "green"],
+    "yellow-orange": [prim[0], "orange"],
     "orange": [prim[1], prim[0]],
-    "blue-green": [prim[2], prim[0]]
+    "red-orange": [prim[1], "orange"],
 }
 
 count = int(input())
 
-for i in range(count):
+for line in range(count):
     colorDest = input()
+    colorsNeeded = []
+    for a in range(2):
+        colorComponent = colors[colorDest][a]
+        if colorComponent in prim:
+            colorsNeeded.append(colorComponent)
+        for b in range(2):
+            colorsNeeded.append
+
     if colorDest in prim:
         print(f"No colors need to be mixed to make {colorDest}.")
     else:
