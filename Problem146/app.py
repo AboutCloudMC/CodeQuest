a4346e1d0a077cfb9bc6989f14b8bb9b18bd2123
AboutCloudMC <aboutcloudmc@gmail.com>
2025-01-27 12:15:19 +0000

    Solve Problem146

--- Problem146/app.py
+++ Problem146/app.py
@@ -1,26 +1,32 @@
 import math
 
 cases = int(input())
-for a in range(1):
+for _ in range(cases):
     launches = int(input())
+
     lines = []
-    for i in range(launches):
-        lines.append(input()) 
-    print(lines)
+    for _ in range(launches):
+        lines.append(input())
+        
+    current = None
     for line in lines:
-        print(line)
         data = line.split(" ")
+
         #date time cloud wind direction
         date = data[0]
         time = data[1]
         cloud = int(data[2])
         wind_speed = float(data[3])
         wind_dir = math.radians(int(data[4]))
+
         wind_ew = wind_speed * math.sin(wind_dir)
         wind_ns = wind_speed * math.cos(wind_dir)
-        if (cloud > 1000) or (wind_ns > 20) or (wind_ew > 40):
-            print("ABORT LAUNCH")
+
+        if cloud <= 1000 and abs(wind_ns) <= 20 and abs(wind_ew) <= 40:
+            current = f"{date} {time}"
             break
-        else:
-            print(f"{date} {time}")
-            break
+
+    if current is None:
+        print("ABORT LAUNCH")
+    else:
+        print(current)
