a001840ff726b265972793e06dd345cbe8d6cc02
AboutCloudMC <aboutcloudmc@gmail.com>
2025-02-05 13:02:40 +0000

    Continue hard problem

--- Problem153/app.py
+++ Problem153/app.py
@@ -1,19 +1,24 @@
-
-
 cases = int(input())
 
 for _ in range(cases):
     inp = input().split()
-    rows = inp[0]
-    dives = inp[1]
+    rows = int(inp[0])
+    dives = int(inp[1])
     table = []
+    diveTable = []
+    
     for a in range(rows):
-        table[a][0] = int(input())
-        table[a][1] = int(input())
-        table[a][2] = int(input())
-        table[a][3] = int(input())
-
-
-
-
-
+        row = input().split()
+        table.append([int(row[0]), int(row[1]), row[2], row[3]])
+    
+    print(table)
+    for _ in range(dives):
+        inp = input().split()
+        depth = int(inp[0])
+        time = int(inp[1])
+        diveTable.append([depth, time])
+        
+        for b in range(len(table)):
+            if table[b][0] >= depth and table[b][1] >= time:
+                print(table[b][2], table[b][3])
+    print(diveTable)
