1672e507c699b5640805f40cf28a72bdf68e67f3
AboutCloudMC <aboutcloudmc@gmail.com>
2025-02-27 11:03:44 +0000

    Start Problem 208

---
+++ Problem208/app.py
@@ -0,0 +1,9 @@
+
+cases = int(input())
+
+for _ in range(cases):
+    variance = {}
+    items = int(input())
+    estimates = input().split()
+    actual = input().split()
+    for i in range(items):
